edd529836c19c0edde3f38ca51ca207eeeb3aae5
Silmon121 <simonmandak@seznam.cz>
2024-06-06 21:21:49 +0200

    more songs

--- Levels/Tutorial_level/dojoDoorArea.gd
+++ Levels/Tutorial_level/dojoDoorArea.gd
@@ -8,5 +8,5 @@ func _on_area_exited(area):
 	if(area.name == "hurtBox"):
 		insideArea = false
 func _process(delta):
-	if(insideArea and Input.is_action_just_pressed("interact")):
+	if(insideArea and Input.is_action_just_pressed("interact") and globalChange.changeLevel):
 		get_tree().change_scene_to_file("res://Levels/Tutorial_level/tutorialLevelGarden.tscn")
